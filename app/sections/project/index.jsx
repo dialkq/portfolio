e41b5123bdb73ef1bfcbeb4226d1badb07ef098d
dialkq <dialkq@gmail.com>
2024-07-14 23:02:37 +0700

    add rute-krl to new project

--- app/sections/project/index.jsx
+++ app/sections/project/index.jsx
@@ -2,6 +2,7 @@ import { domAnimation, LazyMotion } from "framer-motion";
 import { HeadingDivider } from "components";
 import Image from "next/image";
 import cinemart from "../../../public/assets/images/cinemart.png";
+import rutekrl from "../../../public/assets/images/rutekrl.png";
 import Link from "next/link";
 
 export function ProjectsSection() {
@@ -10,7 +11,8 @@ export function ProjectsSection() {
 			<section id="projects" className="section">
 				<HeadingDivider title="Latest projects" />
 				{/* PROJECT LIST */}
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
+					{/* CINEMART */}
 					<div
 						className="flex flex-col w-full my-5 md:my-8 lg:my-10 mx-auto rounded-md
 				 border-2 border-emerald-300 hover:border-emerald-500 shadow-md p-5"
@@ -43,6 +45,42 @@ export function ProjectsSection() {
 								</div>
 							</Link>
 							<Link href="https://cinemart.vercel.app/" target="_blank">
+								<div
+									className="font-mono bg-emerald-400 hover:bg-emerald-500 py-2 px-3 md:px-4
+							text-white font-bold rounded-md cursor-pointer"
+								>
+									Visit
+								</div>
+							</Link>
+						</div>
+					</div>
+
+					{/* RUTE-KRL */}
+					<div
+						className="flex flex-col w-full my-5 md:my-8 lg:my-10 mx-auto rounded-md
+				 border-2 border-emerald-300 hover:border-emerald-500 shadow-md p-5"
+					>
+						<p className="font-mono font-bold text-center text-xl">Rute-krl</p>
+						<Image className="mx-auto mt-5" src={rutekrl} alt="rute-krl" width={350} height={350} />
+						<div className="my-5 w-full mx-auto">
+							<p className="font-sans font-semibold">
+								Rute-krl is a simple & elegant Indonesian public train commuter line app, featuring
+								select and save station locally and get real-time schedule. Rute-krl built with
+								Next.js and deployed to Vercel. Why web? Because it easy to use, no need to install
+								it at playstore/appstore, and no need login to access it.
+							</p>
+						</div>
+
+						<div className="flex justify-evenly my-5">
+							<Link href="https://github.com/dialkq/rute-krl" target="_blank">
+								<div
+									className="font-mono bg-emerald-400 hover:bg-emerald-500 py-2 px-3 md:px-4 
+							text-white font-bold rounded-md cursor-pointer"
+								>
+									Github
+								</div>
+							</Link>
+							<Link href="https://rute-krl.vercel.app/" target="_blank">
 								<div
 									className="font-mono bg-emerald-400 hover:bg-emerald-500 py-2 px-3 md:px-4
 							text-white font-bold rounded-md cursor-pointer"
